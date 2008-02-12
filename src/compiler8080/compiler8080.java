22710404921c57029f7c976fea77b285860a2077
Peter Jakubčo <pjakubco@gmail.com>
2008-02-12 10:41:38 +0000

    Update info about version and Copyright

--- src/compiler8080/compiler8080.java
+++ src/compiler8080/compiler8080.java
@@ -80,8 +80,8 @@ public class compiler8080 implements ICompiler {
     }
     
     public String getName() { return "Intel 8080 Compiler"; }
-    public String getVersion() { return "0.1b"; }
-    public String getCopyright() { return "\u00A9 Copyright 2007, Peter Jakubčo"; }
+    public String getVersion() { return "0.2b"; }
+    public String getCopyright() { return "\u00A9 Copyright 2007-2008, Peter Jakubčo"; }
     public String getDescription() {
         return "It is a clone of original Intel's assembler. For syntax look"
                 + " at users manual.";
