9e87cb622ce4ed931750629846990ba416967078
Peter Jakubčo <pjakubco@gmail.com>
2010-05-01 16:02:04 +0200

    * Add meta-info into project properties file * Add licensing information into each file (GNU GPL 2)

--- src/gui/StudioFrame.java
+++ src/gui/StudioFrame.java
@@ -2,6 +2,22 @@
  * StudioFrame.java
  *
  * Created on Nedeľa, 2007, august 5, 13:43
+ *
+ * Copyright (C) 2007-2010 Peter Jakubčo <pjakubco at gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package gui;
