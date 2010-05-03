08d6c50797901637ca5706a171bf2095e9515a39
Peter Jakubčo <pjakubco@gmail.com>
2010-05-03 07:49:28 +0200

    * Updated license info in each source file (GNU GPL 2) * Fix fields names in NiceButton

--- src/sio88/CpuPort2.java
+++ src/sio88/CpuPort2.java
@@ -6,6 +6,23 @@
  *
  * This is the data port of 88-SIO card.
  *
+ * Copyright (C) 2008-2010 Peter Jakubčo <pjakubco at gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * ----------------------------------------------------------------------------
  * A read to the data port gets the buffered character, a write
  * to the data port writes the character to the device.
  *
--- src/sio88/PhysicalPort.java
+++ src/sio88/PhysicalPort.java
@@ -1,12 +1,26 @@
 /*
  * PhysicalPort.java
  *
- * RS-232 physical port
- * 
  * Created on 28.7.2008, 8:22:30
  * hold to: KISS, YAGNI
  *
- * Male plug
+ * Male plug, RS-232 physical port
+ *
+ * Copyright (C) 2008-2010 Peter Jakubčo <pjakubco at gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
 package sio88;
