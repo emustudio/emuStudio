8f4b78bc5afe05646e2bbfea4dc96d3fcec4e8b0
Peter Jakubčo <pjakubco@gmail.com>
2010-05-03 16:17:32 +0200

    Updated license info in each source file (GNU GPL 2)

--- src/standard_mem/gui/utils/memoryTableModel.java
+++ src/standard_mem/gui/utils/memoryTableModel.java
@@ -4,9 +4,25 @@
  * Created on Streda, 2007, marec 21, 9:38
  *
  * KEEP IT SIMPLE STUPID
- *
  * YOU AREN'T GONNA NEED IT
  *
+ * Copyright (C) 2007-2010 Peter Jakubčo <pjakubco at gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * ----------------------------------------------------------------------------
  * tabulka ma 16 riadkov a 16 stlpcov (od 0 po FF)
  * celkovo tak zobrazuje 16*16 = 256 hodnot na stranku
  *
