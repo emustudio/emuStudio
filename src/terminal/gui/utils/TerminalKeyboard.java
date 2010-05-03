ad73012a04d8fcd72c16252c35406d7cbdc0a868
Peter Jakubčo <pjakubco@gmail.com>
2010-05-03 08:14:28 +0200

    * Updated license info in each source file (GNU GPL 2) * Fix fields names in NiceButton * Added javadoc for TerminalKeyboard.java

--- src/terminal/gui/utils/TerminalKeyboard.java
+++ src/terminal/gui/utils/TerminalKeyboard.java
@@ -1,3 +1,24 @@
+/**
+ * TerminalKeyboard.java
+ *
+ * KISS, YAGNI
+ *
+ * Copyright (C) 2009-2010 Peter Jakubčo <pjakubco at gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
 package terminal.gui.utils;
 
 import java.awt.Component;
@@ -12,7 +33,7 @@ import terminal.TerminalFemale;
 public class TerminalKeyboard extends java.awt.event.KeyAdapter implements ContainerListener {
 	private TerminalFemale female;
     private boolean halfDuplex = false;
-    private Object keyLock; // monitor for key pressing
+    private final Object keyLock; // monitor for key pressing
     private int keyCode = 0;
     private TerminalDisplay terminal;
     
