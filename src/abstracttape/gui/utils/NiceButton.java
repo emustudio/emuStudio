2939eaaf0d6602bf83fa22677853bc1637d19e45
Peter Jakubčo <pjakubco@gmail.com>
2010-05-03 08:06:36 +0200

    * Updated license info in each source file (GNU GPL 2) * Fix fields names in NiceButton

--- src/abstracttape/gui/utils/NiceButton.java
+++ src/abstracttape/gui/utils/NiceButton.java
@@ -1,9 +1,23 @@
 /**
  * NiceButton.java
  * 
- * (c) Copyright 2009, P. Jakubčo
- * 
  *  KISS, YAGNI
+ *
+ * Copyright (C) 2009-2010 Peter Jakubčo <pjakubco at gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 package abstracttape.gui.utils;
 
@@ -13,15 +27,15 @@ import javax.swing.JButton;
 
 @SuppressWarnings("serial")
 public class NiceButton extends JButton {
-	private final static int WIDTH = 95;
-	private final static int HEIGHT = 30;
+	private final static int x_WIDTH = 95;
+	private final static int x_HEIGHT = 30;
 
 	public NiceButton() {
 		super();
 		Dimension d = getPreferredSize();
-		d.setSize(WIDTH,HEIGHT); //d.getHeight());
+		d.setSize(x_WIDTH,x_HEIGHT); //d.getHeight());
 		this.setPreferredSize(d);
-		this.setSize(WIDTH, HEIGHT);//this.getHeight());
+		this.setSize(x_WIDTH, x_HEIGHT);//this.getHeight());
 		this.setMinimumSize(d);
 		this.setMaximumSize(d);
 	}
