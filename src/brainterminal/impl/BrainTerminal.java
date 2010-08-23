c3ecfb391a3a0fdec419f413e63ef68a946ae0d8
Peter Jakubčo <pjakubco@gmail.com>
2010-08-23 11:15:08 +0200

    Updated all code for the new emuLib version (7.0)

--- src/brainterminal/impl/BrainTerminal.java
+++ src/brainterminal/impl/BrainTerminal.java
@@ -21,105 +21,97 @@
  */
 package brainterminal.impl;
 
-import braincpu.interfaces.IBrainCPUContext;
 import brainterminal.gui.BrainTerminalDialog;
 import plugins.ISettingsHandler;
-import plugins.cpu.ICPUContext;
-import plugins.device.IDevice;
 import plugins.device.IDeviceContext;
-import plugins.memory.IMemoryContext;
+import plugins.device.SimpleDevice;
+import runtime.Context;
 import runtime.StaticDialogs;
+import braincpu.interfaces.C35E1D94FC14C94F76C904F09494B85079660C9BF;
 
-public class BrainTerminal implements IDevice {
-	private final static String BRAIN_CPU_CONTEXT = "d32e73041bf765d98eea1b8664ef6b5e"; 
-	private long hash;
-	private IBrainCPUContext cpu;
-	private BrainTerminalContext terminal; 
-	private BrainTerminalDialog gui;
-	
-	public BrainTerminal(Long hash) {
-		this.hash = hash;
-		gui = new BrainTerminalDialog();
-		terminal = new BrainTerminalContext(gui);
-	}
+public class BrainTerminal extends SimpleDevice {
 
-	@Override
-	public String getTitle() {
-		return "BrainDuck terminal";
-	}
+    private C35E1D94FC14C94F76C904F09494B85079660C9BF cpu;
+    private BrainTerminalContext terminal;
+    private BrainTerminalDialog gui;
 
-	@Override
-	public String getVersion() {
-		return "0.11-rc1";
-	}
+    public BrainTerminal(Long pluginID) {
+        super(pluginID);
+        gui = new BrainTerminalDialog();
+        terminal = new BrainTerminalContext(gui);
+        Context.getInstance().register(pluginID, terminal, IDeviceContext.class);
+    }
 
-	@Override
-	public String getCopyright() {
-		return "\u00A9 Copyright 2009-2010, P. Jakubčo";
-	}
+    @Override
+    public String getTitle() {
+        return "BrainDuck terminal";
+    }
 
-	@Override
-	public String getDescription() {
-		return "Simple terminal device for BrainDuck architecture";
-	}
-	
-	@Override
-	public boolean initialize(ICPUContext cpu, IMemoryContext mem,
-			ISettingsHandler settings) {
-		if (!(cpu instanceof IBrainCPUContext)
-				|| !cpu.getHash().equals(BRAIN_CPU_CONTEXT) ) {
-			StaticDialogs.showErrorMessage("BrainTerminal doesn't support this CPU");
-			return false;
-		}
-		this.cpu = (IBrainCPUContext)cpu;
-		this.cpu.attachDevice(terminal);
-		// read settings
-		
-	    String s = settings.readSetting(hash, "verbose");
-	    if (s.toUpperCase().equals("TRUE")) {
-	    	gui.setVerbose(true);
-	    	gui.setVisible(true);
-	    } else
-	    	gui.setVerbose(false);
-		return true;
-	}
-	
-	@Override
-	public void reset() {
-		// zmažeme obrazovku
-		gui.clearScreen();
-	}
+    @Override
+    public String getVersion() {
+        return "0.11-rc2";
+    }
 
-	@Override
-	public void destroy() {
-		gui.dispose();
-		gui = null;
-	}
+    @Override
+    public String getCopyright() {
+        return "\u00A9 Copyright 2009-2010, P. Jakubčo";
+    }
 
-	@Override
-	public long getHash() { return hash; }
-	
-	@Override
-	public IDeviceContext getNextContext() {
-		return terminal;
-	}
+    @Override
+    public String getDescription() {
+        return "Simple terminal device for BrainDuck architecture";
+    }
 
-	@Override
-	public boolean attachDevice(IDeviceContext device) {
-		return true;
-	}
+    @Override
+    public boolean initialize(ISettingsHandler settings) {
+        super.initialize(settings);
 
-	@Override
-	public void detachAll() { }
+        cpu = (C35E1D94FC14C94F76C904F09494B85079660C9BF)Context.getInstance()
+                .getCPUContext(pluginID,
+                C35E1D94FC14C94F76C904F09494B85079660C9BF.class);
+        if (cpu == null) {
+            StaticDialogs.showErrorMessage("BrainTerminal needs to be connected"
+                    + "to the BrainCPU.");
+            return false;
+        }
+        cpu.attachDevice(terminal);
 
-	@Override
-	public void showGUI() {
-		gui.setVisible(true);
-	}
+        // read settings
+        String s = settings.readSetting(pluginID, "verbose");
+        if (s.toUpperCase().equals("TRUE")) {
+            gui.setVerbose(true);
+            gui.setVisible(true);
+        } else {
+            gui.setVerbose(false);
+        }
+        return true;
+    }
 
-	@Override
-	public void showSettings() { 
-		// nemáme GUI s nastaveniami
-	}
+    @Override
+    public void reset() {
+        // zmažeme obrazovku
+        gui.clearScreen();
+    }
 
+    @Override
+    public void destroy() {
+        gui.dispose();
+        gui = null;
+    }
+   
+
+    @Override
+    public void showGUI() {
+        gui.setVisible(true);
+    }
+
+    @Override
+    public void showSettings() {
+        // nemáme GUI s nastaveniami
+    }
+
+    @Override
+    public boolean isShowSettingsSupported() {
+        return false;
+    }
 }
--- src/brainterminal/impl/BrainTerminalContext.java
+++ src/brainterminal/impl/BrainTerminalContext.java
@@ -21,42 +21,36 @@
  */
 package brainterminal.impl;
 
-import java.util.EventObject;
 import brainterminal.gui.BrainTerminalDialog;
 import plugins.device.IDeviceContext;
 
 public class BrainTerminalContext implements IDeviceContext {
-	private BrainTerminalDialog gui;
-	
-	public BrainTerminalContext(BrainTerminalDialog gui) {
-		this.gui = gui;
-	}
 
-	@Override
-	public Class<?> getDataType() {
-		return Short.class;
-	}
+    private BrainTerminalDialog gui;
 
-	@Override
-	public Object in(EventObject evt) {
-	    return (short)gui.getChar();
-	}
+    public BrainTerminalContext(BrainTerminalDialog gui) {
+        this.gui = gui;
+    }
 
-	@Override
-	public void out(EventObject evt, Object val) {
-		short s = (Short)val;
-		char c = (char)s;
-		gui.putChar(c);
-	}
+    @Override
+    public Class<?> getDataType() {
+        return Short.class;
+    }
 
-	@Override
-	public String getHash() {
-		return "4a0411686e1560c765c1d6ea903a9c5f";
-	}
+    @Override
+    public Object read() {
+        return (short) gui.getChar();
+    }
 
-	@Override
-	public String getID() {
-		return "brain-terminal-context";
-	}
+    @Override
+    public void write(Object val) {
+        short s = (Short) val;
+        char c = (char) s;
+        gui.putChar(c);
+    }
 
+    @Override
+    public String getID() {
+        return "brain-terminal-context";
+    }
 }
