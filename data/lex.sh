3e2ed31700f0ae6321957e92701016413c24058a
Peter Jakubčo <pjakubco@gmail.com>
2011-10-31 14:30:14 +0100

    Improved file names and parser generators in data directory

--- data/lex.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#~/bin/jflex-1.4.3/bin/jflex Lexer8080.flex
-d:/bin/jflex-1.4.3/bin/jflex Lexer8080.flex
--- data/parser.sh
+++ data/parser.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-source="Parser8080.cup" # nazov zdrojoveho suboru
+source="parser.cup" # nazov zdrojoveho suboru
 target="Parser8080"     # nazov triedy parsera
 symbols="Sym8080"        # nazov triedy symbolov
 package="as_8080.impl"   # aky balik ma mat parser aj symboly
 
 echo "Generating JAVA files..."
-#java -jar ~/bin/java-cup/java-cup-11a.jar -package $package -parser $target -symbols $symbols -interface $source
+java -jar ~/bin/java-cup/java-cup-11a.jar -package $package -parser $target -symbols $symbols -interface $source
 
-java -jar d:/bin/java-cup/java-cup-11a.jar -package $package -parser $target -symbols $symbols -interface $source
+#java -jar d:/bin/java-cup/java-cup-11a.jar -package $package -parser $target -symbols $symbols -interface $source
