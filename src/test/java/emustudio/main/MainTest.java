f425be06b8172c81376bc1b12da69a6e58907bd6
Peter Jakubčo <pjakubco@gmail.com>
2012-03-18 13:24:50 +0100

    Fixed POM file and add one test

---
+++ src/test/java/emustudio/main/MainTest.java
@@ -0,0 +1,41 @@
+/*
+ * MainTest.java
+ *
+ * (c) Copyright 2012, P. Jakubčo <pjakubco@gmail.com>
+ */
+package emustudio.main;
+
+import java.util.Date;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author vbmacher
+ */
+public class MainTest extends TestCase {
+    
+    public MainTest(String testName) {
+        super(testName);
+    }
+    
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+    
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+    
+    public void testPassword() {
+        String password = emulib.runtime.Context.SHA1(String.valueOf(Math.random())
+                + new Date().toString());
+        assertTrue(emulib.runtime.Context.assignPassword(password));
+        password = emulib.runtime.Context.SHA1(String.valueOf(Math.random())
+                + new Date().toString());
+        assertFalse(emulib.runtime.Context.assignPassword(password));
+    }
+    
+    
+}
