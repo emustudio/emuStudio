0077ebfbd6680c43a96ada9b5f6d4d4cb8d302e1
Michal Šipoš <michal.sipos.2@student.tuke.sk>
2016-01-16 20:15:28 +0100

    update: again, only integer numbers supported

---
+++ RASPMemory/src/sk/tuke/emustudio/rasp/memory/NumberMemoryItem.java
@@ -0,0 +1,35 @@
+package sk.tuke.emustudio.rasp.memory;
+
+/**
+ * Value as a memory item, either value of register, or instruction operand.
+ *
+ * @author miso
+ */
+public class NumberMemoryItem implements MemoryItem {
+
+    private final int value;
+
+    /**
+     * Constructor.
+     *
+     * @param value the value
+     */
+    public NumberMemoryItem(int value) {
+        this.value = value;
+    }
+
+    /**
+     * Get the value.
+     *
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
+
+}
