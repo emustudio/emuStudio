501674c6a9645c4c3d2f417ec668979310647816
Michal Šipoš <michal.sipos.2@student.tuke.sk>
2016-05-12 18:50:30 +0200

    typo

--- RASPMemory/src/sk/tuke/emustudio/rasp/memory/impl/RASPMemoryImpl.java
+++ RASPMemory/src/sk/tuke/emustudio/rasp/memory/impl/RASPMemoryImpl.java
@@ -99,11 +99,11 @@ public class RASPMemoryImpl extends AbstractMemory {
     @Override
     public String getVersion() {
         return "";
-    } 
+    }
 
     @Override
     public int getProgramStart() {
         return context.getProgramStart();
-    } 
-    
+    }
+
 }
