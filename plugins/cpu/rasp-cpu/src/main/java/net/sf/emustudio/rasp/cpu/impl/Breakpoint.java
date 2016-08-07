0b61fb22ba06f981207df415a2631c5b83adac89
Michal Šipoš <michalsipos.ke@gmail.com>
2016-08-07 20:57:30 +0200

    [#116] Migrated RASP CPU project build system from Ant to Maven

---
+++ plugins/cpu/rasp-cpu/src/main/java/net/sf/emustudio/rasp/cpu/impl/Breakpoint.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.sf.emustudio.rasp.cpu.impl;
+
+/**
+ * Breakpoint in debugger; it is an exception, so, can be thrown.
+ *
+ * @author miso
+ */
+public class Breakpoint extends Exception {
+
+    public Breakpoint() {
+    }
+
+}
