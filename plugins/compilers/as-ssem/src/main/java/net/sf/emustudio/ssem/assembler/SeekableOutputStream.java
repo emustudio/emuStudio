da83ba83ec28cf96c32569bce6b628010b7abbaf
Peter Jakubčo <peter.jakubco@zoomint.com>
2016-10-08 22:02:24 +0200

    [#127] Add missing SeekableOutputStream class

---
+++ plugins/compilers/as-ssem/src/main/java/net/sf/emustudio/ssem/assembler/SeekableOutputStream.java
@@ -0,0 +1,10 @@
+package net.sf.emustudio.ssem.assembler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public abstract class SeekableOutputStream extends OutputStream {
+
+    public abstract void seek(int position) throws IOException;
+
+}
