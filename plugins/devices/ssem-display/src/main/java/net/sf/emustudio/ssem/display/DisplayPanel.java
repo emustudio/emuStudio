b3671fe5a62bc42c17eb2179fdfed427c9e86d6b
Peter Jakubčo <peter.jakubco@zoomint.com>
2017-01-05 16:28:32 +0100

    [#129] Added first implementation of CRT device

---
+++ plugins/devices/ssem-display/src/main/java/net/sf/emustudio/ssem/display/DisplayPanel.java
@@ -0,0 +1,100 @@
+/*
+ * KISS, YAGNI, DRY
+ *
+ * (c) Copyright 2006-2016, Peter Jakubčo
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package net.sf.emustudio.ssem.display;
+
+import emulib.runtime.NumberUtils;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.util.stream.Collectors;
+import javax.swing.JPanel;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class DisplayPanel extends JPanel {
+    private final static int PIXEL_SIZE = 10;
+    private final static int GAP = 2;
+
+    private final boolean[][] memory = new boolean[32][32];
+
+    public DisplayPanel() {
+        super.setBackground(Color.BLACK);
+        super.setDoubleBuffered(true);
+    }
+
+    public void stop() {
+        clear();
+    }
+    
+    public void writeRow(Byte[] value, int row) {
+        int number = NumberUtils.readInt(value, NumberUtils.Strategy.REVERSE_BITS);
+        Boolean[] bits = String.format("%32s", Integer.toBinaryString(number)).chars()
+                .mapToObj(c -> c == '1')
+                .collect(Collectors.toList())
+                .toArray(new Boolean[0]);
+        
+        for (int i = 0; i < 32; i++) {
+            memory[row][i] = bits[i];
+        }
+        repaint();
+    }
+
+    public void clear() {
+        for (boolean[] memoryRow : memory) {
+            for (int j = 0; j < memoryRow.length; j++) {
+                memoryRow[j] = false;
+            }
+        }
+        repaint();
+    }
+
+     @Override
+    public void paintComponent(Graphics g) {
+        Dimension size = getSize();
+        int startX = size.width / 2 - 16 * (PIXEL_SIZE + GAP);
+        int startY = size.height / 2 - 16 * (PIXEL_SIZE + GAP);
+
+        g.setColor(Color.BLACK);
+        g.fillRect(0, 0, size.width, size.height);
+        
+        for (int i = 0; i < memory.length; i++) {
+            for (int j = 0; j < memory[i].length; j++) {
+                if (memory[i][j]) {
+                  g.setColor(Color.GREEN);
+                  g.fillRect(
+                          startX + i * (PIXEL_SIZE + GAP),
+                          startY + j * (PIXEL_SIZE + GAP),
+                          PIXEL_SIZE,
+                          PIXEL_SIZE
+                  );
+                } else {
+                  g.setColor(Color.WHITE);
+                  g.drawRect(
+                          startX + i * (PIXEL_SIZE + GAP),
+                          startY + j * (PIXEL_SIZE + GAP),
+                          PIXEL_SIZE,
+                          PIXEL_SIZE
+                  );
+                }
+            }
+        }
+    }
+
+}
---
+++ plugins/devices/ssem-display/src/main/java/net/sf/emustudio/ssem/display/DisplaySSEM.java
@@ -0,0 +1,104 @@
+/*
+ * KISS, YAGNI, DRY
+ *
+ * (c) Copyright 2006-2016, Peter Jakubčo
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+package net.sf.emustudio.ssem.display;
+
+import emulib.annotations.PLUGIN_TYPE;
+import emulib.annotations.PluginType;
+import emulib.emustudio.SettingsManager;
+import emulib.plugins.device.AbstractDevice;
+import emulib.plugins.memory.MemoryContext;
+import emulib.runtime.ContextPool;
+import emulib.runtime.exceptions.PluginInitializationException;
+
+import java.util.MissingResourceException;
+import java.util.Objects;
+import java.util.ResourceBundle;
+
+@PluginType(
+        type = PLUGIN_TYPE.DEVICE,
+        title = "SSEM CRT display",
+        copyright = "\u00A9 Copyright 2006-2016, Peter Jakubčo",
+        description = "CRT display for SSEM computer."
+)
+@SuppressWarnings("unused")
+public class DisplaySSEM extends AbstractDevice {
+    private boolean nogui;
+    private final ContextPool contextPool;
+    private DisplayDialog display;
+
+    public DisplaySSEM(Long pluginID, ContextPool contextPool) {
+        super(pluginID);
+        this.contextPool = Objects.requireNonNull(contextPool);
+    }
+
+    @Override
+    public String getVersion() {
+        try {
+            ResourceBundle bundle = ResourceBundle.getBundle("net.sf.emustudio.ssem.display.version");
+            return bundle.getString("version");
+        } catch (MissingResourceException e) {
+            return "(unknown)";
+        }
+    }
+
+    @Override
+    public void initialize(SettingsManager settings) throws PluginInitializationException {
+        super.initialize(settings);
+        MemoryContext<Byte> memory = contextPool.getMemoryContext(pluginID, MemoryContext.class);
+        
+        if (memory.getDataType() != Byte.class) {
+            throw new PluginInitializationException(this, "Expected Byte memory cell type!");
+        }
+
+        String s = settings.readSetting(pluginID, SettingsManager.NO_GUI);
+        nogui = (s != null) && s.toUpperCase().equals("TRUE");
+
+        if (!nogui) {
+            display = new DisplayDialog(memory);
+        }
+    }
+
+    @Override
+    public void reset() {
+        display.reset();
+    }
+
+    @Override
+    public void destroy() {
+        display.dispose();
+    }
+
+    @Override
+    public void showGUI() {
+        if (display != null) {
+            display.setVisible(true);
+        }
+    }
+
+    @Override
+    public void showSettings() {
+        // we don't have settings GUI
+    }
+
+    @Override
+    public boolean isShowSettingsSupported() {
+        return false;
+    }
+}
