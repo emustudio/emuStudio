7a8f1d8afd36e29b3755f9bfb29582933ec20c66
Peter Jakubčo <peter.jakubco@zoomint.com>
2017-03-03 16:39:53 +0100

    [#138] Introduce fixurl.sh

---
+++ docs/fixurl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo Deleting generated sites
+find _docuser/ -type f -regex ".*\.\(html\)" -exec rm {} \;
+find _docdevel/ -type f -regex ".*\.\(html\)" -exec rm {} \;
+
+echo Building site...
+jekyll clean
+JEKYLL_ENV=production jekyll build --verbose
+
+echo Copying results...
+cp -rf _site/docuser/* _docuser/
+cp -rf _site/docdevel/* _docdevel/
+
+
+echo Fixing URLs...
+find _docuser/ -type f -print0 | xargs -0 sed -i 's_http://github\.com/pages/vbmacher/emuStudio_https://vbmacher.github.io/emuStudio_g'
+find _docdevel/ -type f -print0 | xargs -0 sed -i 's_http://github\.com/pages/vbmacher/emuStudio_https://vbmacher.github.io/emuStudio_g'
+
+echo Fixing image URLs...
+find _docuser/ -type f -print0 | xargs -0 sed -i 's_<img src="/docuser/_<img src="https://vbmacher.github.io/emuStudio/docuser/_g'
+find _docdevel/ -type f -print0 | xargs -0 sed -i 's_<img src="/docdevel/_<img src="https://vbmacher.github.io/emuStudio/docdevel/_g'
+
+echo Checking images...
+find _docuser/ -type f -regex ".*\.\(html\)" -print0 | xargs -0 grep "{imagepath}"
+find _docdevel/ -type f -regex ".*\.\(html\)"  -print0 | xargs -0 grep "{imagepath}"
+
+
+
+
