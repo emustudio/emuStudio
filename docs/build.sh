d20fb4c92602b5518482bd9f3c58a0ee0a4ebba5
Peter Jakubčo <peter.jakubco@zoomint.com>
2017-03-06 10:50:41 +0100

    [#138] Modified build.sh

--- docs/build.sh
+++ docs/build.sh
@@ -14,9 +14,9 @@ cp -rf _site/docuser/* _docuser/
 cp -rf _site/docdevel/* _docdevel/
 cp -r _site/images images/
 
-#echo Fixing URLs...
-#find _docuser/ -type f -print0 | xargs -0 sed -i 's_http://github\.com/pages/vbmacher/emuStudio_https://vbmacher.github.io/emuStudio_g'
-#find _docdevel/ -type f -print0 | xargs -0 sed -i 's_http://github\.com/pages/vbmacher/emuStudio_https://vbmacher.github.io/emuStudio_g'
+echo Fixing URLs...
+find _docuser/ -type f -print0 | xargs -0 sed -i 's_http://github\.com/pages/vbmacher/emuStudio_https://vbmacher.github.io/emuStudio_g'
+find _docdevel/ -type f -print0 | xargs -0 sed -i 's_http://github\.com/pages/vbmacher/emuStudio_https://vbmacher.github.io/emuStudio_g'
 
 echo Fixing image URLs...
 find _docuser/ -type f -print0 | xargs -0 sed -i 's_<img src="/docuser/_<img src="https://vbmacher.github.io/emuStudio/docuser/_g'
