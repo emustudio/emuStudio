56187e41e826e9d8fe934e7e890748ff2585f3af
Peter Jakubčo <pjakubco@gmail.com>
2018-01-27 13:18:38 +0100

    Updated google analytics & fixed URLs

--- docs/build.sh
+++ docs/build.sh
@@ -7,22 +7,22 @@ rm -rf ./images/
 
 echo Building site...
 jekyll clean
-JEKYLL_ENV=production jekyll build --verbose --baseurl '/emuStudio'
+JEKYLL_ENV=production jekyll build --verbose # --baseurl '/emuStudio'
 
 echo Copying results...
 cp -rf _site/docuser/* _docuser/
 cp -rf _site/docdevel/* _docdevel/
 cp -r _site/images images/
 
-echo Fixing URLs...
-find _docuser/ -type f -print0 | xargs -0 sed -i 's_http://github\.com/pages/vbmacher/emuStudio_https://vbmacher.github.io/emuStudio_g'
-find _docdevel/ -type f -print0 | xargs -0 sed -i 's_http://github\.com/pages/vbmacher/emuStudio_https://vbmacher.github.io/emuStudio_g'
+#echo Fixing URLs...
+#find _docuser/ -type f -print0 | xargs -0 sed -i 's_http://github\.com/pages/vbmacher/emuStudio_https://vbmacher.github.io/emuStudio_g'
+#find _docdevel/ -type f -print0 | xargs -0 sed -i 's_http://github\.com/pages/vbmacher/emuStudio_https://vbmacher.github.io/emuStudio_g'
 
-echo Fixing image URLs...
-find _docuser/ -type f -print0 | xargs -0 sed -i 's_<img src="/docuser/_<img src="https://vbmacher.github.io/emuStudio/docuser/_g'
-find _docuser/ -type f -print0 | xargs -0 sed -i 's_<img src="/images/_<img src="https://vbmacher.github.io/emuStudio/images/_g'
-find _docdevel/ -type f -print0 | xargs -0 sed -i 's_<img src="/docdevel/_<img src="https://vbmacher.github.io/emuStudio/docdevel/_g'
-find _docdevel/ -type f -print0 | xargs -0 sed -i 's_<img src="/images/_<img src="https://vbmacher.github.io/emuStudio/images/_g'
+#echo Fixing image URLs...
+#find _docuser/ -type f -print0 | xargs -0 sed -i 's_<img src="/docuser/_<img src="https://vbmacher.github.io/emuStudio/docuser/_g'
+#find _docuser/ -type f -print0 | xargs -0 sed -i 's_<img src="/images/_<img src="https://vbmacher.github.io/emuStudio/images/_g'
+#find _docdevel/ -type f -print0 | xargs -0 sed -i 's_<img src="/docdevel/_<img src="https://vbmacher.github.io/emuStudio/docdevel/_g'
+#find _docdevel/ -type f -print0 | xargs -0 sed -i 's_<img src="/images/_<img src="https://vbmacher.github.io/emuStudio/images/_g'
 
 echo Checking images...
 find _docuser/ -type f -regex ".*\.\(html\)" -print0 | xargs -0 grep "{imagepath}"
