b266665e9bf94b6e2829f6f8ca4f8a006d97dfaa
Peter Jakubco <pjakubco@gmail.com>
2023-02-23 12:24:51 +0100

    [#315, #257] Fix Altair8800 bootloader sources

--- application/src/main/files/examples/altair8800/boot/mdbl.asm
+++ application/src/main/files/examples/altair8800/boot/mdbl.asm
@@ -1,118 +1,132 @@
-; reverse-engineered modified disk boot loader (mdbl.bin) from simh emulator
+; Modified Disk Boot Loader (MDBL)
+; reverse-engineered from simh emulator
+; only use with simh-formatted disk images
 
-org 0FF00h
+org 0xFF00
 
-di
-ld b, 80h
+di             ; Disable interrupts
+mvi b, 0x80
+mvi a, 0x0E    ; Reset the SIMH pseudo device
 
-ld a, 0Eh
+reset:
+out 0xFE
+dcr b
+jnz reset      ; Do it for 128 times
 
-l1:
-out (0FEh), a
-dec b
-jp nz, l1
+mvi a, 0x16    ; Stop timer interrupts
+out 0xFE
+mvi a, 0x12    ; Determines whether machine has banked memory
+out 0xFE
+in 0xFE        ; Number of banks
+ora a          ; zero=no banks
+jz copy_ourselves
 
-ld a, 16h
-out (0FEh), a
-ld a, 12h
-out (0FEh), a
+mvi a, 0x0C    ; Select bank
+out 0xFE
+xra a          ; bank=0
+out 0xFE
 
-in a, (0FEh)
-or a
-jp z, l2
-ld a, 0Ch
-out (0FEh), a
-xor a
-out (0FEh), a
+copy_ourselves:
+lxi h, 0x5C00  ; destination address
+lxi d, start   ; source address
+mvi c, 0x88    ; 136 times (bytes)
 
-l2:
-ld hl, 5C00h
-ld de, l4
-ld c, 88h
+do_copy:
+ldax d         ; A <- source
+mov m, a       ; dest <- A
+inx d
+inx h
+dcr c
+jnz do_copy
+jmp 0x5C00
 
-l3:
-ld a, (de)
-ld (hl), a
-inc de
-inc hl
-dec c
-jp nz, l3
-jp 5C00h
+; ORG 0x5C00   ; cannot do that in emuStudio
+start:         ; at 0x5C00
+lxi sp, 0x5D21
+mvi a, 0       ; drive 0
+out 0x8        ; select (track = sector = 0)
+mvi a, 4       ; head load
+out 0x9
+jmp 0x5C19
 
-l4:
-ld sp, 5D21h
-ld a, 0
-out (8h), a
-ld a, 4
-out (9h), a
-jp 5C19h
+in 0x8         ; at 0x5C0E; read status port
+ani 2          ; when 0, head movement allowed
+jnz 0x5C0E     ; repeat until head movement allowed
 
-in a, (8h)
-and 2
-jp nz, 5C0Eh
-ld a, 2
-out (9h), a
-in a, (8h)
-and 40h
-jp nz, 5C0Eh
-ld de, 0h
-ld b, 8
-push bc
-push de
+mvi a, 2       ; head out
+out 0x9
+in 0x8         ; at 0x5C19; read status port
+ani 0x40       ; when 0, indicates head is on track 0
+jnz 0x5C0E     ; repeat until head is on track 0
 
-ld de, 8086h
-ld hl, 5C88h
+lxi d, 0
+mvi b, 0x8     ; boot sector number
 
-in a, (9h)
-rra
-jp c, 5C2Dh
-and 1Fh
+push b         ; at 0x5C25
+push d
 
-cp b
-jp nz, 5C2Dh
-in a, (8h)
-or a
-jp m, 5C39h
-in a, (0Ah)
+lxi d, 0x8086  ; data read count: D=unimportant, E=data count (0x86 = 134 bytes = 1 sector)
+lxi h, 0x5C88  ; destination where to read disk data
 
-ld (hl), a
-inc hl
-dec e
-jp nz, 5C39h
-pop de
-ld hl, 5C8Bh
-ld b, 80h
-ld a, (hl)
-ld (de), a
-inc hl
-inc de
-dec b
-jp nz, 5C4Dh
-pop bc
-ld hl, 5C00h
-ld a, d
-cp h
-jp nz, 5C60h
-ld a, e
-cp l
-jp nc, 5C80h
-inc b
-inc b
+in 0x9         ; at 0x5C2D; next sector if head is loaded, read sector number
+rar            ; port2: x x n n n n n t; where x-unused, n-sector number, t-sector true
+jc 0x5C2D      ; repeat until "sector true"
 
-ld a, b
-cp 20h
-jp c, 5C25h
-ld b, 1
-jp z, 5C25h
+ani 0x1F       ; get just sector number
+cmp b          ; is it the one we requested?
+jnz 0x5C2D     ; if not, repeat with next sector
 
-in a, (8h)
-and 2h
-jp nz, 5C70h
-ld a, 1
-out (9h), a
-ld b, 0
-jp 5C25h
-ld a, 80h
-out (8h), a
-ei
-jp 0
+in 0x8         ; at 0x5C39; read status port
+ora a          ; test 0x80 bit (sign flag will be set in this case)
+jm 0x5C39      ; When bit7=0, indicates that read circuit has new byte to read, otherwise repeat
+
+in 0x0A        ; read data from disk
+mov m, a       ; (hl) <- data byte
+inx h
+dcr e          ; count--
+jnz 0x5C39     ; if count!=0, repeat
+
+pop d          ; DE=0
+lxi h, 0x5C8B  ; source address (where the sector is loaded), ignore first 3 bytes
+mvi b, 0x80    ; data count (=128 bytes sector)
+
+mov a, m       ; at 0x5C4D; A <- (HL)
+stax d         ; (DE) <- A
+inx h
+inx d
+dcr b
+jnz 0x5C4D     ; repeat until 128 bytes are read
+
+pop b          ; B=sector number
+lxi h, 0x5C00  ; our code start address
+mov a, d       ; latest dest address high byte (where the copy ended so far)
+cmp h          ; are we crossing 0x5C00 boundary?
+jnz 0x5C60     ; jump if H != D
+
+mov a, e       ; latest dest address low byte
+cmp l          ; are we crossing 0x00 boundary ? (only if D=0x5C)
+
+jnc 0x5C80     ; at 0x5C60 - CY=H>D or (H=D and L>E); if H<=D or (H=D and L<=E) - we're DONE.
+               ; that means we are copying at least 5C00 data since initially D=0
+inr b          ; sector++
+inr b          ; sector++ (continue skewed sector line)
+mov a, b
+cpi 0x20       ; sector < 32 ?
+jc 0x5C25      ; if yes, jump to read next sector
+
+mvi b, 1       ; new sector number (new skewed sector line)
+jz 0x5C25      ; sector == 32 (the last one); if yes jump to read sector "line" starting at B
+
+in 0x8         ; at 0x5C70; here the sector number > 32 (must increment track); read status port
+ani 2          ; when 0, head movement is allowed
+jnz 0x5C70     ; repeat until head movement is allowed
+
+mvi a, 1       ; head in (track++; sector=0)
+out 0x9
+mvi b, 0       ; new sector number (new skewed sector line)
+jmp 0x5C25     ; read the "line"
+
+mvi a, 0x80    ; at 0x5C80; unselect drive
+out 0x8
+ei             ; enable interrupts
+jmp 0          ; jump to CP/M "kernel"
