0db4837142af7182bcf1bbbe756717517b886464
Peter Jakubco <pjakubco@gmail.com>
2025-08-14 17:59:17 +0200

    [#217] Replace GPL3 file headers with SPDX headers

--- plugins/cpu/z80-cpu/src/test/java/net/emustudio/plugins/cpu/zilogZ80/Z80Tests.java
+++ plugins/cpu/z80-cpu/src/test/java/net/emustudio/plugins/cpu/zilogZ80/Z80Tests.java
@@ -1,3 +1,5 @@
+/* SPDX-FileCopyrightText: 2006-2025 Peter Jakubčo
+   SPDX-License-Identifier: GPL-3.0-or-later */
 package net.emustudio.plugins.cpu.zilogZ80;
 
 import net.emustudio.plugins.cpu.zilogZ80.suite.ByteTestBuilder;
