d6228ccb14169c1453b0deec9d11a76b82572a60
Peter Jakubco <pjakubco@gmail.com>
2025-08-14 17:59:17 +0200

    [#217] Replace GPL3 file headers with SPDX headers

--- plugins/device/vt100-terminal/src/test/java/net/emustudio/plugins/device/vt100/api/ContextVt100Test.java
+++ plugins/device/vt100-terminal/src/test/java/net/emustudio/plugins/device/vt100/api/ContextVt100Test.java
@@ -1,3 +1,5 @@
+/* SPDX-FileCopyrightText: 2006-2025 Peter Jakubčo
+   SPDX-License-Identifier: GPL-3.0-or-later */
 package net.emustudio.plugins.device.vt100.api;
 
 import net.emustudio.emulib.plugins.device.DeviceContext;
